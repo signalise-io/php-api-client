ef70f0dbff6db8c0eb12e2fb8b5a414c795ea4d8
Ndottens <nick@elgentos.nl>
2022-11-22 09:36:58 +0100

    first commit

--- src/Client/ApiClient.php
+++ src/Client/ApiClient.php
@@ -12,6 +12,7 @@ namespace Signalise\PhpClient\Client;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Psr\Http\Message\ResponseInterface;
+use Signalise\PhpClient\enum\CallMethods;
 use Signalise\PhpClient\Exception\ResponseException;
 use Signalise\PhpClient\Traits\FailedResponse;
 
@@ -19,15 +20,18 @@ class ApiClient
 {
     use FailedResponse;
 
+    private const SIGNALISE_POST_CONNECTS         = 'api/v1/connects';
+    private const SIGNALISE_GET_CONNECTS          = 'api/v1/connects';
+    private const SIGNALISE_POST_ORDER_HISTORY    = 'api/v1/connects/{{connectId}}/history';
+    private const SIGNALISE_GET_HISTORY_STATUS    = 'api/v1/connects/{{connectId}}/history/status';
+    private const CREATE_CONNECT_CONTENT_TYPE     = 'application/x-www-form-urlencoded';
+    private const POST_ORDER_HISTORY_CONTENT_TYPE = 'application/json';
+
     private Client $client;
 
     private string $apiKey;
 
     private string $apiUrl;
-
-    private const SIGNALISE_GET_CONNECTS       = 'api/v1/connects';
-    private const SIGNALISE_POST_ORDER_HISTORY = 'api/v1/connects/{{connectId}}/history';
-    private const SIGNALISE_GET_HISTORY_STATUS = 'api/v1/connects/{{connectId}}/history/status';
 
     public function __construct(Client $client)
     {
@@ -40,12 +44,12 @@ class ApiClient
         $this->apiKey = $apiKey;
     }
 
-    private function getHeaders(): array
+    private function getHeaders(string $contentType): array
     {
         return [
             'User-Agent' => 'ApiClient / PHP ' . phpversion(),
             'Accept' => 'application/json',
-            'Content-Type' => 'application/json',
+            'Content-Type' => $contentType,
             'Authorization' => sprintf('Bearer %s', $this->apiKey)
         ];
     }
@@ -59,20 +63,25 @@ class ApiClient
         );
     }
 
+    private function createUrl(string $call): string
+    {
+        return sprintf(
+            '%s/%s',
+            rtrim($this->apiUrl, '/'),
+            $call
+        );
+    }
+
     /**
      * @throws GuzzleException
      */
     private function get(string $call): ResponseInterface
     {
         return $this->client->request(
-            'GET',
-            sprintf(
-                '%s/%s',
-                rtrim($this->apiUrl, '/'),
-                $call
-            ),
+            CallMethods::GET_METHOD,
+            $this->createUrl($call),
             [
-                'headers' => $this->getHeaders()
+                'headers' => $this->getHeaders(self::POST_ORDER_HISTORY_CONTENT_TYPE)
             ]
         );
     }
@@ -83,10 +92,10 @@ class ApiClient
     private function post(string $serializedData, string $connectId): ResponseInterface
     {
         return $this->client->request(
-            'POST',
+            CallMethods::POST_METHOD,
             $this->createConnectIdUri($connectId, self::SIGNALISE_POST_ORDER_HISTORY),
             [
-                'headers' => $this->getHeaders(),
+                'headers' => $this->getHeaders(self::POST_ORDER_HISTORY_CONTENT_TYPE),
                 'body' => $serializedData
             ]
         );
@@ -139,6 +148,33 @@ class ApiClient
         $this->setUp($apiUrl, $apiKey);
 
         $response = $this->post($serializedData, $connectId);
+
+        self::unableProcessResponse($response);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    /**
+     * @throws GuzzleException|ResponseException
+     */
+    public function createConnect(
+        string $apiUrl,
+        string $apiKey,
+        array $formData
+    ): array {
+        $this->setUp($apiUrl, $apiKey);
+
+        $response = $this->client->request(
+            CallMethods::POST_METHOD,
+            $this->createUrl(self::SIGNALISE_POST_CONNECTS),
+            [
+                'headers' => $this->getHeaders(self::CREATE_CONNECT_CONTENT_TYPE),
+                'form_params' => [
+                    'name' => $formData['name'],
+                    'type' => $formData['type']
+                ]
+            ]
+        );
 
         self::unableProcessResponse($response);
 
---
+++ src/enum/CallMethods.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Copyright Elgentos BV. All rights reserved.
+ * https://www.elgentos.nl/
+ */
+
+declare(strict_types=1);
+
+namespace Signalise\PhpClient\enum;
+
+abstract class CallMethods
+{
+    public const POST_METHOD = 'POST';
+    public const GET_METHOD  = 'GET';
+}
